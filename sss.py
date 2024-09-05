77786f34cabbef0e5b305541e3396420353f9092
sunil2662 <sunilnaikoti2662@gmail.com>
2024-09-05 15:30:38 +0530

    sunny1

--- sss.py
+++ sss.py
@@ -2,3 +2,6 @@ a=10
 b=20
 print(a+b)
 print("hi")
+print("hi")
+print("hi")
+print("hi")
--- zzz.py
+++
@@ -1,8 +0,0 @@
-a=10
-b=20
-print(a-b)
-print("hello")
-print("hello")
-print("hello")
-print("hello")
-print("hello")
